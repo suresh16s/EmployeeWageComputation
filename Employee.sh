91d1bfe488d5293c09db6f57cd476073c86fe9c4
suresh16s <sureshthite48@gmail.com>
2020-07-09 23:06:47 +0530

    daily wage with total wage

--- Employee.sh
+++ Employee.sh
@@ -1,16 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash  
 echo "Welcome to employee wage computation program"
+#constants
 Wage_per_hours=20
 Working_days_per_month=20
 Absent=0
 Full_time=1
 Part_time=2
+#variables
 Total_wage=0
 Total_working_hrs=0
 Total_working_days=0
-function Getworkhours () {
-	local CheckEmp=$1
-	case $CheckEmp in
+declare -A Dailywage
+declare -A Totalwage
+
+function getWorkHours () {
+	local Checkemp=$1
+	case $Checkemp in
 		$Full_time)
 			Emp_hrs=8
 			;;
@@ -26,10 +31,17 @@ echo $Emp_hrs
 while [[ $Total_working_hrs -lt 100 && $Total_working_days -lt 20 ]]
 do
 	isPresent=$(( RANDOM % 3  ))
-	((Total_working_days++))
-	Working_hrs=$( Getworkhours $isPresent )
-	Daily_wage=$(( $Wage_per_hours * $Working_hrs ))
-	Total_wage=$(( $Total_wage + $Daily_wage ))
+	Working_hrs=$( getWorkHours $isPresent )
+	Daily_wage[Total_working_days]=$(( $Wage_per_hours * $Working_hrs ))
+	Total_wage=$(( $Total_wage + ($Wage_per_hours * $Working_hrs)  ))
+	Total_wage[$Total_working_days]=$Total_wage
 	Total_working_hrs=$(( $Total_working_hrs + $Working_hrs ))
+	((Total_working_days++))
 done
-echo "Total wage is:"$Total_wage
+
+echo "Daily wages" - "Total Wages "
+for (( i=0; i<$Total_working_days; i++ ))
+do
+	echo ${Daily_wage[$i]}"- "${Total_wage[$i]}
+done
+

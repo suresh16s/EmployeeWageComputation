5050d16f02ccb40e88210eaeb2847f920b07da23
suresh16s <sureshthite48@gmail.com>
2020-07-09 23:02:19 +0530

    using function

--- Employee.sh
+++ Employee.sh
@@ -1,38 +1,35 @@
 #!/bin/bash -x
 echo "Welcome to employee wage computation program"
-#variables
-Wage_Per_Hours=20
-Working_Hours=8
-Part_Time_Hours=4
+Wage_per_hours=20
+Working_days_per_month=20
 Absent=0
-Full_Time=1
-Part_Time=2
-Total_Wage=0
-Total_Working_Hrs=0
-Total_Working_Days=0
-
-while [[ $Total_Working_Hrs -lt 100 && $Total_Working_Days -lt 20 ]]
+Full_time=1
+Part_time=2
+Total_wage=0
+Total_working_hrs=0
+Total_working_days=0
+function Getworkhours () {
+	local CheckEmp=$1
+	case $CheckEmp in
+		$Full_time)
+			Emp_hrs=8
+			;;
+		$Part_time)
+			Emp_hrs=4
+			;;
+		$Absent)
+			Emp_hrs=0
+			;;
+	esac
+echo $Emp_hrs
+}
+while [[ $Total_working_hrs -lt 100 && $Total_working_days -lt 20 ]]
 do
 	isPresent=$(( RANDOM % 3  ))
-	((Total_Working_Days++))
-	case $isPresent in
-		$Full_Time)
-		echo "Employee is Present full time"
-		Daily_Wage=$(( $Wage_Per_Hours * $Working_Hours ))
-		Total_Wage=$(( $Total_Wage + $Daily_Wage ))
-		Total_Working_Hrs=$(( $Total_Working_Hrs + $Working_Hours ))
-		;;
-		$Part_Time)
-		echo "employee is  Present partime"
-		Daily_Wage=$(( $Wage_Per_Hours * $Part_Time_Hours ))
-		Total_Wage=$(( $Total_Wage + $Daily_Wage ))
-		Total_Working_Hrs=$(( $total_Working_Hrs + $Working_Hours ))
-		;;
-		$Absent)
-		echo "Employee is Absent"
-		Daily_Wage=0
-		Total_Wage=$(( $Total_Wage + $Daily_Wage ))
-		;;
-	esac
+	((Total_working_days++))
+	Working_hrs=$( Getworkhours $isPresent )
+	Daily_wage=$(( $Wage_per_hours * $Working_hrs ))
+	Total_wage=$(( $Total_wage + $Daily_wage ))
+	Total_working_hrs=$(( $Total_working_hrs + $Working_hrs ))
 done
-echo "Total wage is:" $Total_Wage
+echo "Total wage is:"$Total_wage

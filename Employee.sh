9b76129b4fc3e8411c27c912b11ee13b13348f03
suresh16s <sureshthite48@gmail.com>
2020-07-09 22:47:23 +0530

    using case

--- Employee.sh
+++ Employee.sh
@@ -1,20 +1,25 @@
-#!/bin/bash 
+
+#!/bin/bash -x
+#variables
 Wage_per_hour=20
 Full_day_hour=8
 Part_time_hour=4
-isPresent=$(( RANDOM%3 ))
-if [ $isPresent -eq 1 ]
-then
-        echo "Employee is Present:"
-        Daily_wage=$(( $Wage_per_hour * $Full_day_hour ))
-elif [ $isPresent -eq 2 ]
-then
-	echo "Employee is part time Present:"
-        Daily_wage=$(( $Wage_per_hour* $Part_time_hour ))
+Full_time=1
+Half_time=2
+Absent=0
 
-elif [ $isPresent -eq 0 ]
-then
-	echo "Employee is Absent:"
-	Daily_wage=0
-fi
+isPresent=$(( RANDOM%3 ))
+case $isPresent in
+	$Full_time)
+        echo "Employee is Present:"
+        Daily_wage=$(( $Wage_per_hour * $Full_day_hour )) ;;
+
+	$Half_time)
+        echo "Employee is part time Present:"
+        Daily_wage=$(( $Wage_per_hour* $Part_time_hour )) ;;
+
+	$Absent)
+        echo "Employee is Absent:"
+        Daily_wage=0
+esac
 echo "The Daily wage is:$Daily_wage"

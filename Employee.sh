fe2e48572fb0a488ecca6b8a6049f01f718963bf
suresh16s <sureshthite48@gmail.com>
2020-07-09 22:37:24 +0530

    part time

--- Employee.sh
+++ Employee.sh
@@ -1,14 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash 
 Wage_per_hour=20
 Full_day_hour=8
-isPresent=$(( RANDOM%2 ))
+Part_time_hour=4
+isPresent=$(( RANDOM%3 ))
 if [ $isPresent -eq 1 ]
 then
-	echo "Employee is Present:"
-	Daily_wage=$(( $Wage_per_hour * $Full_day_hour ))
+        echo "Employee is Present:"
+        Daily_wage=$(( $Wage_per_hour * $Full_day_hour ))
+elif [ $isPresent -eq 2 ]
+then
+	echo "Employee is part time Present:"
+        Daily_wage=$(( $Wage_per_hour* $Part_time_hour ))
+
 elif [ $isPresent -eq 0 ]
 then
 	echo "Employee is Absent:"
-	Daily_wage=0;
+	Daily_wage=0
 fi
 echo "The Daily wage is:$Daily_wage"
